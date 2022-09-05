4116263bcf526db232ed3c62b3356ea5c7fd57d0
en3sis <940170+en3sis@users.noreply.github.com>
2022-09-05 17:09:39 +0200

    Adds /mock command, from the famouse meme

---
+++ src/commands/mock.ts
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { CommandInteraction } from 'discord.js'
+
+// https://discord.js.org/#/docs/main/stable/class/CommandInteraction?scrollTo=replied
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('mock')
+    .setDescription('MoCks YoUr TeXt')
+    .addStringOption((string) =>
+      string.setName('text').setDescription('Your sentance to be mocked').setRequired(true)
+    ),
+  async execute(interaction: CommandInteraction) {
+    const text = interaction.options.getString('text')
+
+
+    await interaction.channel.send(text.split('').map((letter, i) => (i % 2) == 0 ? letter.toUpperCase() : letter.toLowerCase()).join(''))
+    await interaction.channel.send('<:mock:1016362569088376924>')
+
+    await interaction.reply({ content: 'Done :P', ephemeral: true })
+  },
+}
