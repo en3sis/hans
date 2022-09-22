59694b1fb2efaf339d54a26e636d1666724001dc
en3sis <940170+en3sis@users.noreply.github.com>
2022-09-22 14:57:40 +0200

    commits missing files

---
+++ tests/utils/crypto.spec.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import { base64, base64Decode } from './../../src/utils/crypto'
+
+describe('Utils: Crypto', () => {
+  context('base64()', () => {
+    it('should encode a string', () => {
+      const bytes = base64('904719402044383273')
+      expect(bytes).to.eql('OTA0NzE5NDAyMDQ0MzgzMjcz')
+    })
+
+    it('should encode an object', () => {
+      const bytes = base64({ name: 'subreddit' })
+      expect(bytes).to.eql('eyJuYW1lIjoic3VicmVkZGl0In0=')
+    })
+
+    it('should decode a string', () => {
+      const bytes = base64Decode('OTA0NzE5NDAyMDQ0MzgzMjcz')
+      expect(bytes).to.eql('904719402044383273')
+    })
+
+    it('should decode an object', () => {
+      const bytes = base64Decode('eyJ0ZXN0IjoidGVzdCJ9')
+      expect(bytes).to.eql('{"test":"test"}')
+    })
+  })
+})
