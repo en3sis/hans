d95bb57e5681a71e6c59a8a9a7d6a57df2db9edf
en3sis <940170+en3sis@users.noreply.github.com>
2022-12-16 09:15:33 +0100

    Starting with the default prompt

--- src/controllers/plugins/chat-gpt3.controller.ts
+++ src/controllers/plugins/chat-gpt3.controller.ts
@@ -1,18 +1,24 @@
 import { Client, Message } from 'discord.js'
 import { OpenAiAPI } from '../../lib/open-ai'
 
+const prompt = `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, with a huge affection for gaming, and sometimes sarcastic.
+
+Human: Hello, who are you?
+AI: I am an AI created by OpenAI. How can I help you today?
+Human:`
+
 export const gpt3Controller = async (Hans: Client, message: Message) => {
   const user = message.mentions.users.first()
 
   if (user && user.id === Hans.user?.id) {
     const response = await OpenAiAPI({
-      input: message.content.replace(user.toString(), ''),
+      input: `${prompt} ${message.content.replace(user.toString(), '')}`,
       predicate: '',
       model: 'davinci',
       version: '003',
-      max_tokens: 100,
+      max_tokens: 150,
     })
 
-    return await message.reply({ content: response.data.choices[0].text })
+    return await message.reply({ content: response.data.choices[0].text.replace('AI:', '') })
   }
 }
