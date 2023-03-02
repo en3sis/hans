467e1ab6c02f20f40dadb2d795647c7a65ffc6ae
en3sis <940170+en3sis@users.noreply.github.com>
2023-03-02 19:04:44 +0100

    Enables gpt-3.5-turbo

--- src/lib/open-ai.ts
+++ src/lib/open-ai.ts
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from 'openai'
+import { ChatCompletionResponseMessage, Configuration, OpenAIApi } from 'openai'
 
 const openai = new OpenAIApi(
   new Configuration({
@@ -24,7 +24,7 @@ interface IOpenAIRequestSettings {
  * @returns Promise<string>
  */
 export const OpenAiAPI = async ({
-  model = 'ada',
+  model,
   version = '001',
   predicate,
   input,
@@ -34,15 +34,31 @@ export const OpenAiAPI = async ({
   frequency_penalty = 0.5,
 }: IOpenAIRequestSettings) => {
   try {
-    const response = await openai.createCompletion({
-      model: `text-${model}-${version}`,
-      prompt: `${predicate} \n ${input.split(', ').join('\n')}`,
-      temperature: temperature,
-      max_tokens: max_tokens,
-      top_p: 0.3,
-      presence_penalty,
-      frequency_penalty,
-    })
+    let response: string | ChatCompletionResponseMessage
+    if (model) {
+      const completion = await openai.createCompletion({
+        model: model ? `text-${model}-${version}` : 'gpt-3.5-turbo',
+        prompt: `${predicate} \n ${input.split(', ').join('\n')}`,
+        temperature: temperature,
+        max_tokens: max_tokens,
+        top_p: 0.3,
+        presence_penalty,
+        frequency_penalty,
+      })
+
+      response = completion.data.choices[0].text
+    } else {
+      const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'user', content: 'Who won the world series in 2020?' },
+          { role: 'assistant', content: 'The Los Angeles Dodgers won the World Series in 2020.' },
+          { role: 'user', content: input.split(', ').join('\n') },
+        ],
+      })
+      response = completion.data.choices[0].message.content
+    }
 
     return response
   } catch (error) {
