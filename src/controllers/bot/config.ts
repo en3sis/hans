5d077d4e632fbe85bb3b2047f0498af4c1f6c987
en3sis <940170+en3sis@users.noreply.github.com>
2023-04-28 23:04:04 +0200

    chore: small fixes

--- src/controllers/bot/config.ts
+++ src/controllers/bot/config.ts
@@ -2,35 +2,38 @@ import supabase from '@/lib/supabase'
 import { Database } from '@/types/database.types'
 
 export type BotConfig = Database['public']['Tables']['config']['Row']
-/**
- * Adds the bot configuration, this is for admins only.
+/** Adds the bot configuration, this is for admins only.
  * here you can set things like main discord guild
  */
 export const insertConfiguration = async () => {
   try {
-    const { error } = await supabase.from('config').upsert(
-      {
-        name: 'Hans',
-        discord_client_id: `${process.env.DISCORD_CLIENT_ID}`,
-        bot_guild_id: `${process.env.BOT_GUILD_ID}`,
-        bot_dev_folder: '/bots-playground',
-        activity_type: 4,
-        activity_name: 'VSC',
-        perma_invite: 'https://discord.gg/2tK7PhkZ',
-        disable_commands: [],
-        notify_channel_id: '',
-        website: '',
-        bot_id: `${process.env.DISCORD_CLIENT_ID}`,
-        id: 1,
-      },
-      { onConflict: 'id' },
-    )
+    const { data, error } = await supabase
+      .from('config')
+      .upsert(
+        {
+          name: 'Hans',
+          discord_client_id: `${process.env.DISCORD_CLIENT_ID}`,
+          bot_guild_id: `${process.env.BOT_GUILD_ID}`,
+          bot_dev_folder: '/bots-playground',
+          activity_type: 4,
+          activity_name: 'VSC',
+          perma_invite: 'https://discord.gg/2tK7PhkZ',
+          disable_commands: [],
+          notify_channel_id: '',
+          website: '',
+          bot_id: `${process.env.DISCORD_CLIENT_ID}`,
+          id: 1,
+        },
+        { onConflict: 'id' },
+      )
+      .single()
 
     if (error) {
       throw new Error(error.message)
     }
 
     console.log(`📥  Initial configuration inserted`)
+    return data
   } catch (error) {
     console.log('❌ ERROR: insertConfiguration(): ', error)
   }
@@ -43,7 +46,8 @@ export const getBotConfiguration = async (): Promise<BotConfig> => {
     if (config) {
       return config
     } else {
-      await insertConfiguration()
+      const config = await insertConfiguration()
+      return config
     }
   } catch (error) {
     console.log('❌ ERROR: getBotConfiguration(): ', error)
