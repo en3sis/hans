f744bd60f6f6e24d44e493e006f5e6350429999a
en3sis <940170+en3sis@users.noreply.github.com>
2023-06-12 18:56:05 +0200

    chore: small refactor

--- src/libs/twitch.ts
+++ src/libs/twitch.ts
@@ -26,25 +26,30 @@ interface TwitchStream {
 type TwitchResponse = TwitchUser | TwitchStream
 
 export const getAccessToken = async (): Promise<TwitchAccessToken> => {
-  const tokenResponse = await axios.post(`https://id.twitch.tv/oauth2/token`, null, {
-    params: {
-      client_id: process.env.TWITCH_CLIENT,
-      client_secret: process.env.TWITCH_SECRET,
-      grant_type: 'client_credentials',
-    },
-  })
+  try {
+    const tokenResponse = await axios.post(`https://id.twitch.tv/oauth2/token`, null, {
+      params: {
+        client_id: process.env.TWITCH_CLIENT,
+        client_secret: process.env.TWITCH_SECRET,
+        grant_type: 'client_credentials',
+      },
+    })
 
-  const expiresIn = tokenResponse.data.expires_in
-  const expirationTimestamp = Date.now() + expiresIn * 1000
+    const expiresIn = tokenResponse.data.expires_in
+    const expirationTimestamp = Date.now() + expiresIn * 1000
 
-  setToCache('twitchAccessToken', {
-    accessToken: tokenResponse.data.access_token,
-    expiresAt: expirationTimestamp,
-  })
+    setToCache('twitchAccessToken', {
+      accessToken: tokenResponse.data.access_token,
+      expiresAt: expirationTimestamp,
+    })
 
-  return {
-    accessToken: tokenResponse.data.access_token,
-    expiresAt: expirationTimestamp,
+    return {
+      accessToken: tokenResponse.data.access_token,
+      expiresAt: expirationTimestamp,
+    }
+  } catch (error) {
+    console.log('💢 ERROR: weatherController(): ', error)
+    throw error
   }
 }
 
@@ -60,53 +65,58 @@ export const isTokenExpired = () => {
 }
 
 export const getStreamerInfo = async (username: string): Promise<TwitchResponse> => {
-  let accessTokenData = getFromCache('twitchAccessToken') as TwitchAccessToken
+  try {
+    let accessTokenData = getFromCache('twitchAccessToken') as TwitchAccessToken
 
-  if (!accessTokenData || isTokenExpired()) {
-    const res = await getAccessToken()
+    if (!accessTokenData || isTokenExpired()) {
+      const res = await getAccessToken()
 
-    setToCache('twitchAccessToken', res)
+      setToCache('twitchAccessToken', res)
 
-    accessTokenData = res
-  }
+      accessTokenData = res
+    }
 
-  const response = await axios.get(`${TWITCH_API_URL}/users`, {
-    params: { login: username },
-    headers: {
-      'Client-ID': process.env.TWITCH_CLIENT,
-      Authorization: `Bearer ${accessTokenData.accessToken}`,
-    },
-  })
+    const response = await axios.get(`${TWITCH_API_URL}/users`, {
+      params: { login: username },
+      headers: {
+        'Client-ID': process.env.TWITCH_CLIENT,
+        Authorization: `Bearer ${accessTokenData.accessToken}`,
+      },
+    })
 
-  if (response.data.data.length === 0) {
-    throw new Error('Streamer not found.')
-  }
+    if (response.data.data.length === 0) {
+      throw new Error('Streamer not found.')
+    }
 
-  const userId = response.data.data[0].id
+    const userId = response.data.data[0].id
 
-  const streamResponse = await axios.get(`${TWITCH_API_URL}/streams`, {
-    params: { user_id: userId },
-    headers: {
-      'Client-ID': process.env.TWITCH_CLIENT,
-      Authorization: `Bearer ${accessTokenData.accessToken}`,
-    },
-  })
+    const streamResponse = await axios.get(`${TWITCH_API_URL}/streams`, {
+      params: { user_id: userId },
+      headers: {
+        'Client-ID': process.env.TWITCH_CLIENT,
+        Authorization: `Bearer ${accessTokenData.accessToken}`,
+      },
+    })
 
-  if (streamResponse.data.data.length === 0) {
+    if (streamResponse.data.data.length === 0) {
+      return {
+        username,
+        avatar: response.data.data[0].profile_image_url,
+        description: response.data.data[0].description,
+      }
+    }
+
     return {
       username,
+      title: streamResponse.data.data[0].title,
+      game: streamResponse.data.data[0].game_name,
+      viewers: streamResponse.data.data[0].viewer_count,
+      startedAt: streamResponse.data.data[0].started_at,
       avatar: response.data.data[0].profile_image_url,
-      description: response.data.data[0].description,
+      thumbnail: streamResponse.data.data[0].thumbnail_url,
     }
-  }
-
-  return {
-    username,
-    title: streamResponse.data.data[0].title,
-    game: streamResponse.data.data[0].game_name,
-    viewers: streamResponse.data.data[0].viewer_count,
-    startedAt: streamResponse.data.data[0].started_at,
-    avatar: response.data.data[0].profile_image_url,
-    thumbnail: streamResponse.data.data[0].thumbnail_url,
+  } catch (error) {
+    console.log('💢 ERROR: getStreamerInfo(): ', error)
+    throw error
   }
 }
