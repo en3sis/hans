a7658f6d846bc8769761a6ee26dbe333ebd11fa3
en3sis <940170+en3sis@users.noreply.github.com>
2023-06-12 19:01:50 +0200

    ops: moves the /twitch command to global context

---
+++ src/commands/twitch.ts
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { CommandInteraction } from 'discord.js'
+import { twitchController } from '../controllers/plugins/twitch.controller'
+import { logger } from '../utils/debugging'
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('twitch')
+    .setDescription('Fetches public Twitch profile information & status')
+    .addStringOption((string) =>
+      string.setName('username').setDescription('Twitch username').setRequired(true),
+    ),
+  async execute(interaction: CommandInteraction) {
+    try {
+      const username = interaction.options.get('username').value as string
+
+      const embeds = await twitchController(username)
+
+      interaction.editReply({ ...embeds })
+    } catch (error) {
+      logger('❌ Command: moderation: ', error)
+    }
+  },
+}
