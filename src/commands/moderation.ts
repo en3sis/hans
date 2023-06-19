fb2a4bc22b48517a44d457802bb2a077ee37ecb3
Sorin Curescu <940170+en3sis@users.noreply.github.com>
2023-06-19 17:37:50 +0200

    fix: issue with moderation plugin (#64)
    * fix: issue with moderation plugin

--- src/commands/moderation.ts
+++ src/commands/moderation.ts
@@ -1,14 +1,10 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { CommandInteraction } from 'discord.js'
-import {
-  // enableSentimentAnalysis,
-  purgeMessages,
-} from '../controllers/plugins/moderation.controller'
+import { purgeMessages } from '../controllers/plugins/moderation.controller'
 import { logger } from '../utils/debugging'
 
 // https://discord.js.org/#/docs/main/stable/class/CommandInteraction?scrollTo=replied
-//github.com/discordjs/discord.js/blob/main/packages/builders/docs/examples/Slash%20Command%20Builders.md
-https: module.exports = {
+module.exports = {
   ephemeral: true,
   data: new SlashCommandBuilder()
     .setName('moderation')
@@ -27,48 +23,16 @@ https: module.exports = {
             ),
         ),
     ),
-  // .addSubcommandGroup((group) =>
-  //   group
-  //     .setName('sentiment')
-  //     .setDescription('Plugin: Sentiment Analysis')
-  //     .addSubcommand((subcommand) =>
-  //       subcommand
-  //         .setName('subscribe')
-  //         .setDescription('Subscribe to sentiment analysis notifications')
-  //         .addChannelOption((option) =>
-  //           option
-  //             .setRequired(true)
-  //             .setName('channel')
-  //             .setDescription('Channel for Hans to report'),
-  //         ),
-  //     )
-  //     .addSubcommand((subcommand) =>
-  //       subcommand
-  //         .setName('toggle')
-  //         .setDescription('Enables/Disables the plugin.')
-  //         .addBooleanOption((option) =>
-  //           option
-  //             .setRequired(true)
-  //             .setName('status')
-  //             .setDescription('True for enable, False to disable'),
-  //         ),
-  //     ),
-  // ),
   async execute(interaction: CommandInteraction) {
     try {
       if (!interaction.memberPermissions.has(['Administrator']))
         return interaction.editReply({
           content: 'You do not have permission to use this command',
         })
+
       if (!interaction.isChatInputCommand()) return
 
-      if (interaction.options.getSubcommandGroup() === 'sentiment') {
-        if (interaction.options.getSubcommand() === 'toggle') {
-          // await toggleSentimentAnalysis(interaction)
-        } else {
-          // await enableSentimentAnalysis(interaction)
-        }
-      } else if (interaction.options.getSubcommandGroup() === 'messages') {
+      if (interaction.options.getSubcommandGroup() === 'messages') {
         await purgeMessages(interaction)
       }
     } catch (error) {
