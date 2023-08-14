e5718a9c4ac00a9d145f2db3e5772622075f5443
en3sis <940170+en3sis@users.noreply.github.com>
2023-08-14 16:28:18 +0200

    chore: moves the command to global scope

---
+++ src/commands/timezones.ts
@@ -0,0 +1,51 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { CommandInteraction } from 'discord.js'
+import { timezonesController } from '../controllers/plugins/timezones.controller'
+import { logger } from '../utils/debugging'
+
+module.exports = {
+  ephemeral: true,
+  data: new SlashCommandBuilder()
+    .setName('timezone')
+    .setDescription('Set your timezone & get the time of another user')
+    .setDefaultMemberPermissions('0')
+    .addSubcommand((command) =>
+      command
+        .setName('set')
+        .setDescription('Set your timezone')
+        .addStringOption((option) =>
+          option.setName('zone').setDescription('Your timezone(TZ Identifier)').setRequired(true),
+        ),
+    )
+    .addSubcommand((command) =>
+      command
+        .setName('difference')
+        .setDescription('Shows the differences between your timezone and another user')
+        .addUserOption((option) =>
+          option.setName('user').setDescription('@username').setRequired(true),
+        ),
+    )
+    .addSubcommand((command) =>
+      command.setName('list').setDescription('Lists all the available timezones'),
+    ),
+  async execute(interaction: CommandInteraction) {
+    try {
+      if (!interaction.isChatInputCommand()) return
+
+      if (interaction.options.getSubcommand() === 'list') {
+        return interaction.editReply({
+          embeds: [
+            {
+              title: 'Timezones list',
+              description: `Find your timezone 🔗[here](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones) and copy the data on the **TZ Identifier**`,
+            },
+          ],
+        })
+      } else {
+        await timezonesController(interaction)
+      }
+    } catch (error) {
+      logger('❌ Command: timezone: ', error)
+    }
+  },
+}
