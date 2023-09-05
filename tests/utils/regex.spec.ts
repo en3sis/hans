6e0f6dcdf5b9c972853a1ae82b5f56358f6aa95d
Sorin Curescu <940170+en3sis@users.noreply.github.com>
2023-09-05 17:46:09 +0200

    fix: adds unit test for timezones (#74)

---
+++ tests/utils/regex.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai'
+import { TIMEZONES_LIST } from './../../src/data/timezones'
+import { TIME_ZONES_REGEX } from './../../src/utils/regex'
+
+const validTimeZones = [
+  'America/New_York',
+  'Europe/London',
+  'Asia/Tokyo',
+  'Australia/Sydney',
+  'Africa/Cairo',
+  'America/Los_Angeles',
+  'Europe/Paris',
+  'Pacific/Auckland',
+]
+
+const invalidTimeZones = [
+  'New_York/America',
+  'London/Europe',
+  'Tokyo/Asia',
+  'Sydney/Australia',
+  'Asia/Invalid_Timezone',
+  'Invalid_Timezone/Asia1',
+  'Invalid_Timezone/Invalid_Timezone22',
+]
+
+describe('TIME_ZONES_REGEX', () => {
+  it('should match valid time zones', () => {
+    validTimeZones.forEach((timeZone) => {
+      expect(TIME_ZONES_REGEX.test(timeZone) && TIMEZONES_LIST.includes(timeZone)).eq(true)
+    })
+  })
+
+  it('should not match invalid time zones', () => {
+    invalidTimeZones.forEach((timeZone) => {
+      expect(TIME_ZONES_REGEX.test(timeZone) && TIMEZONES_LIST.includes(timeZone)).eq(false)
+    })
+  })
+})
