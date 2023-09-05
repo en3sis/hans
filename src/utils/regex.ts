1d913d35dfd82f7f15741e267e6674fc42d8ea8d
Sorin Curescu <940170+en3sis@users.noreply.github.com>
2023-09-05 16:37:17 +0200

    fix: regex to allow timezones with format Continent/Country_Name (#73)

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,3 +1,3 @@
 export const ROLE_MENTION_REGEX = /@(\w+)/g
 export const MESSAGE_ID_REGEX = /^(\d{19})$/
-export const TIME_ZONES_REGEX = new RegExp('^([A-Za-z]+/[A-Za-z]+)$')
+export const TIME_ZONES_REGEX = new RegExp('^([A-Za-z]+/[A-Za-z_]+)$')
