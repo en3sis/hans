62b353cb03cc7c5689ff83bf9a361aed47c50c2b
en3sis <940170+en3sis@users.noreply.github.com>
2024-02-13 10:27:03 +0100

    chore: fix Docker build

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -23,13 +23,14 @@ module.exports = {
       // ==-=-=-=-=-=-=-=-=             DEVELOPMENT                    =-=-=-=-=-=-=-= +
       // +=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=+
       // INFO: This is a development feature that allows to run commands without the need of a bot slash command, it will only work in development mode and it will try to run the command from the messageCreate.ts file. Make sure you duplicate and rename messageCreate.template.ts file.
-      if (!!process.env.ISDEV) {
-        const args = message.content.slice(1).trim().split(/ +/g)
-        const command = args.shift().toLowerCase()
+      // TODO: Docker build will fail, look into.
+      // if (!!process.env.ISDEV && process.env.NODE_ENV !== 'production') {
+      //   const args = message.content.slice(1).trim().split(/ +/g)
+      //   const command = args.shift().toLowerCase()
 
-        const module = await import('../controllers/_development/messageCreate')
-        module.default(Hans, message, command, args)
-      }
+      //   const module = await import('../controllers/_development/messageCreate')
+      //   module.default(Hans, message, command, args)
+      // }
     } catch (error) {
       console.log('❌ messageCreate(): ', error)
     }
